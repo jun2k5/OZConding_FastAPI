e9fc1731643895317c143b77122f6ae3e2ea6f95
jun2k5 <jun2k5@naver.com>
2025-12-30 16:20:03 +0900

    오즈코딩 FastAPI 2일차 Introduce CI

---
+++ 02_DAY2/FastAPI인강/testenv/tests/test_temp.py
@@ -0,0 +1,16 @@
+from .temp import add
+
+
+def test_add() -> None:
+    # Given
+    a, b = 1, 1
+
+    # When
+    result = add(a, b)
+
+    # Then
+    assert result == 2
+
+
+# async def test_abc() -> None:
+#     print("abc")
